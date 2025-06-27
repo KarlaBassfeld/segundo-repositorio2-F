f201f516139a7c7d2be8f13981b341596171af36
KarlaBassfeld <karla.bassfeld@escola.pr.gov.br>
2025-06-26 22:08:33 -0300

    Aula 4 26/06

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ function mostraPalavraChave () {
     //alert("Fui clicado!");
     const texto = document.querySelector('#entrada-de-texto').value;   
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palvras = texto.split(" ")
+    const palvras = processaTexto(texto);
 
     campoResultado.textContent = palvras.joins(", ");
+}
+
+function processaTexto(texto) {
+    //let palavras = texto.split(/\s+/); "Retirar espaços"
+    //let palavras = texto.split(/[a-zA-Z]+/);  "letras minusculos e maiusculas"
+    let palavras = texto.split("/\ P{L}=/u/"); /* \P negação; {L} conjunto de letras; + uma ou mais ocorrências; u  Unicode*/
+
+    let  frequencias = [];
+
+    for ( let in palavras) {
+         frequencias [i] = 0;
+         for ( let j in palavras) {
+            if palavras[i] == palavras[j] {
+                frequencias[i]++;
+            }
+         }    
+    }
+
+    console.log(frequencias);
+
+   return palavras;
 }
